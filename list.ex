c754019b46055fa7c3d66eee7c41b5bff91dac52
dmdv <dimos-d@yandex.ru>
2017-03-28 10:36:05 +0300

    List updated

--- list.ex
+++ list.ex
@@ -1,5 +1,13 @@
 defmodule MyList do
+
+    # basecase: an empty list
     def flatten([]), do : []
-    
-     
+
+    # non-empty list with more than one element
+    def flatten([tail | head]) do
+        flatten(head) ++ flatten(tail)
+    end
+
+    # single element list
+    def flatten(head), do : [head]
 end
